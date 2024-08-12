d89807e76818b1ab7ea2c2bd691e88f187fa2210
racso574 <96116063+racso574@users.noreply.github.com>
2024-08-13 01:53:11 +0200

    w

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('about-link').addEventListener('click', function() {
+    // Redirigir a la página "about"
+    window.location.href = 'about.html'; // Cambia esto por la ruta de tu página
+});
+
+document.getElementById('projects-link').addEventListener('click', function() {
+    // Redirigir a la página "projects"
+    window.location.href = 'projects.html'; // Cambia esto por la ruta de tu página
+});
+
+document.getElementById('links-link').addEventListener('click', function() {
+    // Redirigir a la página "links"
+    window.location.href = 'links.html'; // Cambia esto por la ruta de tu página
+});
